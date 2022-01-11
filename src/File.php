a24cbefbf0bec990f75dd9ef0a46598e150cad87
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2022-01-11 15:09:23 +0330

    add insureLocal method

--- src/File.php
+++ src/File.php
@@ -2,11 +2,41 @@
 
 namespace dnj\Filesystem\Tmp;
 
+use dnj\Filesystem\Contracts\IFile;
 use dnj\Filesystem\Local\File as LocalFile;
 
 class File extends LocalFile
 {
     use NodeTrait;
+
+    /**
+	 * @param callable(LocalFile):mixed $callback
+	 */
+	public static function insureLocal(IFile $file, $callback = null): LocalFile {
+		if ($file instanceof LocalFile) {
+			$localFile = $file;
+		} else {
+			$localFile = new self();
+			if ($file->exists()) {
+				$file->copyTo($localFile);
+			}
+		}
+		$originalMd5 = $file->exists() ? $localFile->md5() : null;
+		if ($callback !== null) {
+			call_user_func($callback, $localFile);
+			if ($localFile !== $file) {
+				if ($localFile->exists()) {
+					if ($originalMd5 !== $localFile->md5()) {
+						$localFile->copyTo($file);
+					}
+				} else {
+					$file->delete();
+				}
+			}
+		}
+		
+		return $localFile;
+	}
 
     public function __construct()
     {
