4e7c27e2da937b79b6f8710423693fa592433efa
Hossein Hosni <47793698+hosni@users.noreply.github.com>
2023-01-17 11:40:00 +0330

    Fix codestyle of File.php

--- src/File.php
+++ src/File.php
@@ -10,33 +10,34 @@ class File extends LocalFile
     use NodeTrait;
 
     /**
-	 * @param callable(LocalFile):mixed $callback
-	 */
-	public static function insureLocal(IFile $file, $callback = null): LocalFile {
-		if ($file instanceof LocalFile) {
-			$localFile = $file;
-		} else {
-			$localFile = new self();
-			if ($file->exists()) {
-				$file->copyTo($localFile);
-			}
-		}
-		$originalMd5 = $file->exists() ? $localFile->md5() : null;
-		if ($callback !== null) {
-			call_user_func($callback, $localFile);
-			if ($localFile !== $file) {
-				if ($localFile->exists()) {
-					if ($originalMd5 !== $localFile->md5()) {
-						$localFile->copyTo($file);
-					}
-				} else {
-					$file->delete();
-				}
-			}
-		}
-		
-		return $localFile;
-	}
+     * @param callable(LocalFile):mixed $callback
+     */
+    public static function insureLocal(IFile $file, $callback = null): LocalFile
+    {
+        if ($file instanceof LocalFile) {
+            $localFile = $file;
+        } else {
+            $localFile = new self();
+            if ($file->exists()) {
+                $file->copyTo($localFile);
+            }
+        }
+        $originalMd5 = $file->exists() ? $localFile->md5() : null;
+        if (null !== $callback) {
+            call_user_func($callback, $localFile);
+            if ($localFile !== $file) {
+                if ($localFile->exists()) {
+                    if ($originalMd5 !== $localFile->md5()) {
+                        $localFile->copyTo($file);
+                    }
+                } else {
+                    $file->delete();
+                }
+            }
+        }
+
+        return $localFile;
+    }
 
     public function __construct()
     {
